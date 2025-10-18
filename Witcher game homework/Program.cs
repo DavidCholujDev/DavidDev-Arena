d3f5ec2f7ee05e5028fe0255a8654a33e6b49575
David Choluj <davidcholuj12@gmail.com>
2025-10-18 19:47:49 +0200

    18.10.2025 19.48 . pridana druha area a este neco

--- Witcher game homework/Program.cs
+++ Witcher game homework/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Zdravim ta dobrodruh, Vitaj v mojej krasnej hre, dufam ze si ju uzijes");
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Zdravim ta dobrodruh, Vitaj v mojej krasnej hre, dufam ze si ju uzijes");
 Console.WriteLine("hru vytvoril @ch.d.avid na instagrame");
 Console.WriteLine("enjoy");
 
@@ -9,8 +13,6 @@ const int MonsterHPMax = 100;
 const int PlayerHPLow = 0;
 const int MonsterHPLow = 0;
 const int BossAttackDmg = 41;
-
-
 
 // beggining 
 
@@ -23,34 +25,116 @@ var usrinput = Console.ReadLine();
 
 if (usrinput == "GO")
 {
-    Console.WriteLine($"vyber si utok: Stlac A ak chces pouzit slaby utok, stlac W ak chces pouzit klasicky a stlac D ak chces ultra extra maximum silny utok");
+    Console.WriteLine($"nemas vela sily, mozes pouzit len slaby utok - utok zahajis pismenom A");
     usrinput = Console.ReadLine();
     if (usrinput == "A")
-    // maly damage
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Red;
+        // maly damage
     {
         Console.WriteLine("David HP = 100");
         while (usrinput == "A")
         {
             int damage = 34;
-            int BossHealth = MonsterHPMax - damage;
-            Console.WriteLine($"Hitnuls Davida, ma {BossHealth} HP a ty mas {monsterattack} HP");
+            int BossHealthD = MonsterHPMax - damage;
+            int BossHealthAttackTypeA = MonsterHPMax - 34;
+            Console.WriteLine($"Hitnuls Davida, ma {BossHealthD} HP a ty mas {monsterattack} HP");
             int  healht = playerhealth - BossAttackDmg;
             Console.WriteLine($"David ta trafil do gebule, ostalo ti {healht} HP");
-            break;
-            int i = 90;
+            Console.WriteLine("Hitni Davida, vyber si utok    W - klasika, D - ultra mega silny utok" );
+            usrinput = Console.ReadLine();
+            // druhy utok
+            
+            if (usrinput == "D")
+            {
+                damage = 99;
+                BossHealthD = BossHealthAttackTypeA - damage;
+                if (BossHealthD <= 0)
+                {
+                    Console.WriteLine("porazil si Davida, ides dalej");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                   // boss 2 
+                    Console.WriteLine("Siel si po ulici po ceste z prvej areny, natrafil si na tvoju ucitelku dejepisu Eriku Mikovu. Ta ta ide skusat z dejepisu, musus spravne odpovedat na jej otazky aby si ju porazil");
+                    Console.WriteLine("Erika Mikova - Kedy skoncila prva svatova vojna?");
+                    string answer = Console.ReadLine();
+
+                    if (answer == "1918")
+                    {
+                        Console.WriteLine("Erika Mikova  - Velmi dobre, dalsia otazka - V akom roku prisli Cyril i Metod na Slovensko?");
+                        string ans2 = Console.ReadLine();
+                        if (ans2 == "863")
+                        {
+                            Console.WriteLine("Erika Mikova - Aagh! Az moc sa ti dari, teraz tazka otazka - Kedy Japonci vybuchli Pearl Harbour - Presny datum! *napis ho cely ako napr 07121941 ");
+                            string ans3 = Console.ReadLine();
+                            if (ans3 == "07121941")
+                            {
+                                Console.WriteLine("Erika Mikova - Aagh! Porazil si moje tazke dejepisne otazky teraz ta necham napokoji");
+                            }
+                        }
+                    }
+
+                }
+            }
+            // treti typ utoku
+            
+            if (usrinput == "W")
+            {
+                damage = 66;
+                BossHealthD = BossHealthAttackTypeA - damage;
+                if (BossHealthD <= 0)
+                {
+                    Console.WriteLine("porazil si Davida, ides dalej");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    // boss 2
+                    Console.WriteLine("Siel si po ulici po ceste z prvej areny, natrafil si na tvoju ucitelku dejepisu Eriku Mikovu. Ta ta ide skusat z dejepisu, musus spravne odpovedat na jej otazky aby si ju porazil");
+                    Console.WriteLine("Erika Mikova - Kedy skoncila prva svatova vojna?");
+                    string answer = Console.ReadLine();
+
+                    if (answer == "1918")
+                    {
+                        Console.WriteLine("Erika Mikova  - Velmi dobre, dalsia otazka - V akom roku prisli Cyril i Metod na Slovensko?");
+                        string ans2 = Console.ReadLine();
+                        if (ans2 == "863")
+                        {
+                            Console.WriteLine("Erika Mikova - Aagh! Az moc sa ti dari, teraz tazka otazka - Kedy Japonci vybuchli Pearl Harbour - Presny datum! *napis ho cely ako napr 07121941 ");
+                            string ans3 = Console.ReadLine();
+                            if (ans3 == "07121941")
+                            {
+                                Console.WriteLine("Erika Mikova - Aagh! Porazil si moje tazke dejepisne otazky teraz ta necham napokoji");
+                            }
+                        }
+                    }
+                }
+
+
+                
+                if (healht <= 0)
+                {
+                    Console.WriteLine("David ta porazil, umrel si, ani jedneho bossa si neporazil, slaby si, dovidenia");
+                    
+                }
+
+
+                
+
+                
+                
+            }
 
 
             {
              
             }
 
-        }
+        } 
         
     }   
     
 }
 
-else
-{
-    Console.WriteLine("dovidenia");
-}
+
+
+
+
+
+
